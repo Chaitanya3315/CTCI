fac9560204b503efdc45887104f94474bdce69ec
Chaitanya3315 <tkrishnachaitanya741@gmail.com>
2022-03-15 00:29:12 +0530

    -m2.1

---
+++ StacksAndQueues/Stacks.java
@@ -0,0 +1,66 @@
+package StacksAndQueues;
+import java.util.*;
+
+class FullStackExecption extends Exception{
+
+}
+class EmptyStackException extends Exception{
+
+}
+
+public class Stacks {
+    int[] values;
+    int[] size;
+    int capacity;
+    int noOfStacks=3;
+    Stacks(int size){
+        capacity=size;
+        values=new int[size*noOfStacks];
+        this.size=new int[noOfStacks];
+    }
+    public void push(int num,int value) throws FullStackExecption{
+        if(size[num]==capacity){
+            throw new FullStackExecption();
+        }
+        size[num]++;
+        int index=num*capacity+ size[num]-1;
+        values[index]=value;
+        return;
+    }
+    public int pop(int num)throws EmptyStackException{
+        if(size[num]==0){
+            throw new EmptyStackException();
+        }
+        
+        int index=num*capacity+ size[num]-1;
+        int value=values[index];
+        values[index]=0;
+        size[num]--;
+        return value;
+    }
+
+    public int peek(int num)throws EmptyStackException{
+        if(size[num]==0){
+            throw new EmptyStackException();
+        }
+        int index=num*capacity+ size[num]-1;
+        return size[index];
+    }
+    public boolean isEmpty(int num){
+        return size[num]==0;
+    }
+
+    public static void main(String[] args)throws FullStackExecption{
+        Scanner sc =new Scanner(System.in);
+        int size=sc.nextInt();
+        Stacks st=new Stacks(size);
+        st.push(0,1);
+        st.push(0,1);
+        st.push(0,1);
+        st.push(0,1);
+        st.push(0,1);
+        st.push(1,1);
+        st.push(2,1);
+        System.out.println(st.isEmpty(1));
+    }
+}
