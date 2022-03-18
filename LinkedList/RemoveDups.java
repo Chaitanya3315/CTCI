73e2510871e6d8997fbb7f180334ed362ca369c6
Chaitanya3315 <tkrishnachaitanya741@gmail.com>
2022-03-18 23:14:52 +0530

    -m

---
+++ LinkedList/RemoveDups.java
@@ -0,0 +1,63 @@
+package LinkedList;
+import java.util.*;
+public class RemoveDups {
+    Node head;
+  
+    static class Node { 
+        int data; 
+        Node next; 
+        Node(int d) 
+        { 
+            data = d;
+            next = null; 
+        } 
+    } 
+    public void printList(){
+        Node n= head;
+        while(n!=null)
+        {
+            System.out.println(n.data +" ");
+            n=n.next;
+        }
+    }
+    public void push(int newData){
+        Node newNode = new Node(newData);
+        newNode.next= head;
+         head=newNode;
+    }
+    public void removeDups(Node head){
+        Node temp = head;
+        Node prev=null;
+        HashSet<Integer> m1= new HashSet<Integer>();
+        if(head==null){
+           System.out.println("list is empty");
+        }
+        while(temp.next!=null){
+            if(m1.contains(temp.data)){
+                prev.next=temp.next;
+            }else{
+                m1.add(temp.data);
+                prev=temp;
+            }
+            temp=temp.next;
+        }
+
+    }
+    public static void main(String[] args){
+        RemoveDups obj = new RemoveDups();
+        obj.head = new Node(1); 
+        obj.push(3);
+        obj.push(3);
+        obj.push(2);
+        obj.push(2);
+        obj.push(2);
+        obj.push(3);
+        obj.push(4);
+        obj.printList();
+        obj.removeDups(obj.head);
+        System.out.println("After removing the duplicates");
+        obj.printList();
+
+
+    }
+}
